1fe30c1de7fb6be56edeabd0db434375157007c8
Harshpreet Kaur <harshpreetkaur1403@gmail.com>
2024-09-21 10:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function isSameType(value1, value2) {
-	  if(typeof value1 === typeof value2){
-		  return true 
+	  if(typeof(value1) === typeof(value2)){
+		  return true
 	  }else {
 		  return false
 	  }
